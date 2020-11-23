a85b269b58a4a8d2358fb34d30af4bd16032f551
Joey Heintz <joeheintz45@gmail.com>
2020-11-23 11:49:33 -0600

    updated the database and made the appropriate tables

--- database.sql
+++ database.sql
@@ -4,7 +4,58 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" serial primary key,
+	"username" varchar(50) UNIQUE NOT NULL,
+	"password" varchar(30) NOT NULL
+);
+
+CREATE TABLE "projects" (
+	"id" serial primary key,
+	"project_desc" varchar(400),
+	"project_link" varchar(200) NOT NULL,
+	"profile_id" int references "user"
+);
+
+CREATE TABLE "profile" (
+	"id" serial primary key,
+	"user_id" int references "user",
+	"bio" varchar (350),
+	"display_name" varchar(50),
+	"profile_pic" varchar(200)
+);
+
+CREATE TABLE "collab_post" (
+	"id" serial primary key,
+	"user_id" int references "user",
+	"likes" int,
+	"published" date,
+	"location_id" int references "map"
+);
+
+CREATE TABLE "map" (
+	"id" serial primary key,
+	"location" varchar (60)
+);
+
+CREATE TABLE "messages" (
+	"id" serial primary key,
+	"message" varchar(400)
+);
+
+CREATE TABLE "user_messages" (
+	"id" serial primary key,
+	"primary_user" int references "user",
+	"secondary_user" int references "user",
+	"message_id" int references "messages"
+);
+
+CREATE TABLE "musician_types" (
+	"id" serial primary key,
+	"type" varchar(40)
+);
+
+CREATE TABLE "profile_types" (
+	"id" serial primary key,
+	"profile_id" int references "profile",
+	"type_id" int references "musician_types"
 );
