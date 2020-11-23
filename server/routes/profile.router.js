c70a1ef75b2b6a6f81556d50a2d732c7bb3e15d1
Joey Heintz <joeheintz45@gmail.com>
2020-11-23 16:08:54 -0600

    added the get and post routes for user profiles

--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -10,6 +10,38 @@ const {
  */
 router.get('/', rejectUnauthenticated, (req, res) => {
   // GET route code here
+  const queryText = `SELECT * FROM "profile" 
+    JOIN "profile_types" ON "profile_types".profile_id = "profile".id
+    JOIN "musician_types" ON "musician_types".id = "profile_types".type_id
+    WHERE "user_id" = $1`;
+
+  pool
+    .query(queryText, [req.user.id])
+    .then((dbResponse) => {
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+router.get('/:id', rejectUnauthenticated, (req, res) => {
+  // GET route code here
+  const queryText = `SELECT * FROM "profile" 
+    JOIN "profile_types" ON "profile_types".profile_id = "profile".id
+    JOIN "musician_types" ON "musician_types".id = "profile_types".type_id
+    WHERE "user_id" = $1`;
+
+  pool
+    .query(queryText, [req.params.id])
+    .then((dbResponse) => {
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 /**
@@ -17,6 +49,35 @@ router.get('/', rejectUnauthenticated, (req, res) => {
  */
 router.post('/', rejectUnauthenticated, (req, res) => {
   // POST route code here
+  const queryText = `INSERT INTO "profile" ("bio", "display_name", "user_id", "profile_pic")
+    VALUES ($1, $2, $3, $4)
+    RETURNING "id";`;
+
+  pool
+    .query(queryText, [
+      req.body.bio,
+      req.body.display_name,
+      req.user.id,
+      req.body.profile_pic,
+    ])
+    .then((dbResponse) => {
+      const queryText2 = `INSERT INTO "profile_types" ("profile_id", "type_id")
+        VALUES ($1, $2);`;
+
+      pool
+        .query(queryText2, [dbResponse.rows[0].id, req.body.type_id])
+        .then((dbResponse) => {
+          res.sendStatus(200);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.sendStatus(500);
+        });
+    })
+    .catch((err) => {
+      console.log('/profile error: ', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
