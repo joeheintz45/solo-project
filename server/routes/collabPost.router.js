9f3237d17de2bae56c0b2ab2e4d809de41dcb51b
Joey Heintz <joeheintz45@gmail.com>
2020-11-23 19:07:51 -0600

    added put routes for content and likes on user posts and delete route to delete a post.

--- server/routes/collabPost.router.js
+++ server/routes/collabPost.router.js
@@ -8,11 +8,14 @@ const {
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   // GET route code here
-  const queryText = `SELECT * FROM "collab_post"
+  const queryText = `SELECT "collab_post".id, "collab_post".likes, "collab_post".content, "collab_post".published, 
+    "collab_post".location_id, "profile".id, "profile".display_name, "profile".profile_pic
+    FROM "collab_post"
     JOIN "user" ON "user".id = "collab_post".user_id
-    ORDER BY "published" DESC;`;
+    JOIN "profile" ON "profile".user_id = "user".id
+    ORDER BY "collab_post".published ASC;`;
 
   pool
     .query(queryText)
@@ -40,6 +43,48 @@ router.post('/', rejectUnauthenticated, (req, res) => {
     .query(queryText, queryArray)
     .then((dbResponse) => {
       console.log(dbResponse);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+router.delete('/delete/:id', rejectUnauthenticated, (req, res) => {
+  const queryText = `DELETE FROM "collab_post" WHERE "id"=$1;`;
+
+  pool
+    .query(queryText, [req.params.id])
+    .then((dbResponse) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+router.put('/edit/:id', rejectUnauthenticated, (req, res) => {
+  const queryText = `UPDATE "collab_post" SET "content"=$1 WHERE "id"=$2 AND "user_id"=$3`;
+
+  pool
+    .query(queryText, [req.body.content, req.params.id, req.user.id])
+    .then((dbResponse) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+router.put('/like/:id', rejectUnauthenticated, (req, res) => {
+  const queryText = `UPDATE "collab_post" SET "likes"="likes"+1 WHERE "id"=$1;`;
+
+  pool
+    .query(queryText, [req.params.id])
+    .then((dbResponse) => {
       res.sendStatus(200);
     })
     .catch((err) => {
