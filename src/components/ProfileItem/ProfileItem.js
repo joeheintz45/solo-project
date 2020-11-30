ac9736d4590b07867707fad318948635702c6555
Joey Heintz <joeheintz45@gmail.com>
2020-11-30 00:50:59 -0600

    added the new profile page

--- src/components/ProfileItem/ProfileItem.js
+++ src/components/ProfileItem/ProfileItem.js
@@ -1,9 +1,16 @@
+import { Avatar } from '@material-ui/core';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ProfileItem extends Component {
   render() {
-    return <div>{this.props.item.display_name}</div>;
+    return (
+      <div>
+        <Avatar src="/broken-image.jpg" />
+        {this.props.item.display_name} <br />
+        {this.props.item.bio}
+      </div>
+    );
   }
 }
 
