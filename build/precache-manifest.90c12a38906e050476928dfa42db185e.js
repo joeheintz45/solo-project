85636f73c9bba00c1d051e44b7530604d2fede60
Joey Heintz <joeheintz45@gmail.com>
2020-12-07 16:26:02 -0600

    production build for client

---
+++ build/precache-manifest.90c12a38906e050476928dfa42db185e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06be96171e22ecd11c5e51e9bb11ed8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18876a1cb5d867143227",
+    "url": "/static/css/main.a96b1046.chunk.css"
+  },
+  {
+    "revision": "a9007c36fd925c4cfe2a",
+    "url": "/static/js/2.49387e49.chunk.js"
+  },
+  {
+    "revision": "1e7ec43b130e7e6929d7fb8e1b208114",
+    "url": "/static/js/2.49387e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18876a1cb5d867143227",
+    "url": "/static/js/main.80dd8bae.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
